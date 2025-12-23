a39cc20539ac04a3f32d4801b3d3fae36ccfb9b3
James Hughes <james@pyrosoftsolutions.co.uk>
2025-12-23 03:34:02 +0000

    test: Added more comprehensive tests for internals are internal fixes

--- src/ArchonAnalysers.Tests.Unit/Analyzers/ARCHON001/InternalsAreInternalFixProviderTests.cs
+++ src/ArchonAnalysers.Tests.Unit/Analyzers/ARCHON001/InternalsAreInternalFixProviderTests.cs
@@ -8,22 +8,29 @@ namespace ArchonAnalysers.Tests.Unit.Analyzers.ARCHON001;
 
 public class InternalsAreInternalFixProviderTests
 {
-    [Fact]
-    public async Task FixChangesPublicToInternalOnPublicClass()
+    [Theory]
+    [InlineData("struct")]
+    [InlineData("interface")]
+    [InlineData("record")]
+    [InlineData("enum")]
+    [InlineData("class")]
+    public async Task FixChangesPublicTypeDeclarationsToInternal(string typeKeyword)
     {
-        const string testCode = $$"""
-                                  namespace TestApp.Internal;
-                                  {|{{InternalsAreInternalAnalyzer.DiagnosticId}}:public|} class MyClass;
-                                  """;
+        string testCode = $$"""
+                            namespace TestApp.Internal;
+                            {|{{InternalsAreInternalAnalyzer.DiagnosticId}}:public|} {{typeKeyword}} MyType;
+                            """;
 
-        const string fixedCode = """
-                                 namespace TestApp.Internal;
-                                 internal class MyClass;
-                                 """;
+        string fixedCode = $$"""
+                             namespace TestApp.Internal;
+                             internal {{typeKeyword}} MyType;
+                             """;
 
-
-        CSharpCodeFixTest<InternalsAreInternalAnalyzer, InternalsAreInternalFixProvider, DefaultVerifier> test = new() { TestCode = testCode, FixedCode = fixedCode };
-
+        CSharpCodeFixTest<InternalsAreInternalAnalyzer, InternalsAreInternalFixProvider, DefaultVerifier> test = new()
+        {
+            TestCode = testCode,
+            FixedCode = fixedCode
+        };
 
         await test.RunAsync(TestContext.Current.CancellationToken);
     }
@@ -79,6 +86,32 @@ public class InternalsAreInternalFixProviderTests
             CodeActionEquivalenceKey = $"{InternalsAreInternalAnalyzer.DiagnosticId}:[58..67)", // This is brittle as fuck
             FixedState = { ExpectedDiagnostics = { expectedRemainingDiagnostic }}
         };
+        await test.RunAsync(TestContext.Current.CancellationToken);
+    }
+
+    [Theory]
+    [InlineData("abstract", "class")]
+    [InlineData("sealed", "class")]
+    [InlineData("static", "class")]
+    [InlineData("readonly", "struct")]
+    public async Task FixPreservesNonAccessibilityModifiers(string additionalModifiers, string typeDeclaration)
+    {
+        string testCode = $$"""
+                            namespace TestApp.Internal;
+                            {|{{InternalsAreInternalAnalyzer.DiagnosticId}}:public|} {{additionalModifiers}} {{typeDeclaration}} MyType;
+                            """;
+
+        string fixedCode = $$"""
+                             namespace TestApp.Internal;
+                             {{additionalModifiers}} internal {{typeDeclaration}} MyType;
+                             """;
+
+        CSharpCodeFixTest<InternalsAreInternalAnalyzer, InternalsAreInternalFixProvider, DefaultVerifier> test = new()
+        {
+            TestCode = testCode,
+            FixedCode = fixedCode
+        };
+
         await test.RunAsync(TestContext.Current.CancellationToken);
     }
 }
